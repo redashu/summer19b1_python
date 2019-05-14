873406a5b8ba9694fa0bb6ad83d0261e6eff7bea
Cloud User <ec2-user@ip-172-31-13-212.ap-south-1.compute.internal>
2019-05-14 12:31:36 +0000

    first commit

---
+++ code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+options="""
+Press  1  TO check your Ec2 instance RAM 
+Press  2  TO check since how long  ec2 instance is running 
+Press  3  To check a command  named  tree  and install it 
+Press  4  To check current time and  date 
+Press  5  To login in your facebook account and  update status hello world
+Press  6  To  search something on google 
+Press  7  to  list  all the IP &  MAc in your current  wifi  
+Press  8  to  disconnect  all  wifi connected  device  execpt  YOU  
+PRess  9  to  lauchn on  rhel 7.5  based  freee ec2  instance 
+Press  10  to check status  of  ec2  instance  
+"""
+
+print(options)
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@                             @@@@@")
+print("@@                             @@@@@")
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+# capturing  user input 
+choice=input()
+
+print(choice)
+
